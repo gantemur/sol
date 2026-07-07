73b3260628657d5b02e1c09ffed100f20fd2ef57
gantemur <27515627+gantemur@users.noreply.github.com>
2026-07-07 23:08:21 +0800

    Fix kinetic wave animation

--- tools/kinetic/kinetic.js
+++ tools/kinetic/kinetic.js
@@ -255,7 +255,7 @@ function drawplus() {
                 xm[i][j] += windx*dt;
                 ym[i][j] += windy*dt;
             } else  if (wave) {
-//                if (j==0) x1=-hh2+sinusoid(t); else x1=xm[i][j-1];
+                if (j==0) x1=-hh2+sinusoid(t); else x1=xm[i][j-1];
                 if (j==n2-1) x2=xbox+hh2; else x2=xm[i][j+1];
 //                if (i==0) if (j==0) x1=-hh2+30*Math.sin(t*8);
                 dmean =  x1+x2-2*xm[i][j];
